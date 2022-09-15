2625292d4ac241604daa659fe6f0460d48e05a8e
CGranzell <91188714+CGranzell@users.noreply.github.com>
2022-09-15 08:51:14 +0200

    temp-control-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [temp, setTemp] = useState(10);
+  const [tempColor, setTempColor] = useState("neutral");
+  const [season, setSeason] = useState("background_autumn");
+
+  const increseTemp = () => {
+    if (temp === 30) return;
+
+    const newTemp = temp + 1;
+    if (newTemp >= 25) {
+      setSeason("background_summer");
+      setTempColor("hot");
+    }
+    if (newTemp < 25 && newTemp >= 15) {
+      setSeason("background_spring");
+      setTempColor("neutral");
+    }
+    if (newTemp < 15 && newTemp >= 7) {
+      setSeason("background_autumn");
+      setTempColor("neutral");
+    }
+    if (newTemp < 7) {
+      setSeason("background_winter");
+      setTempColor("cold");
+    }
+    setTemp(newTemp);
+  };
+
+  const decreaseTemp = () => {
+    if (temp === -20) return;
+    const newTemp = temp - 1;
+
+    if (newTemp >= 25) {
+      setSeason("background_summer");
+      setTempColor("hot");
+    }
+    if (newTemp < 25 && newTemp >= 15) {
+      setSeason("background_spring");
+      setTempColor("neutral");
+    }
+    if (newTemp < 15 && newTemp >= 7) {
+      setSeason("background_autumn");
+      setTempColor("neutral");
+    }
+
+    if (newTemp < 7) {
+      setSeason("background_winter");
+      setTempColor("cold");
+    }
+    setTemp(newTemp);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${season}`}>
+      <div className="temp-container">
+        <div className="temperature-display-container">
+          <div className={`temperature-display ${tempColor}`}>{temp}°C</div>
+        </div>
+        <div className="button-container">
+          <button onClick={increseTemp}>+</button>
+          <button onClick={decreaseTemp}>-</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
